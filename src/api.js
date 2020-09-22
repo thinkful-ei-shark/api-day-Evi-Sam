a2e7a3d090b064c24db0a68ab7e1498b1a435313
s-poveda <spoveda545@gmail.com>
2020-09-22 11:57:33 -0400

    wip

---
+++ src/api.js
@@ -0,0 +1,8 @@
+const baseURL = 'https://thinkful-list-api.herokuapp.com/sam-evi/'
+function getItems () {
+    return Promise.resolve('Successful Response')
+}
+
+export default {
+    getItems
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import $ from 'jquery';
 
 import 'normalize.css';
+import api from './api.js';
 import './index.css';
 
 import shoppingList from './shopping-list';
+
+api.getItems()
+.then( res => console.log(res));
 
 const main = function () {
   shoppingList.bindEventListeners();
