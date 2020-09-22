ad5a14a3163e37d205a39fe91bfee2e5cfd41a51
Evisari Riyanto <evisari111012@gmail.com>
2020-09-22 11:28:50 -0700

    step 7

--- src/api.js
+++ src/api.js
@@ -15,7 +15,19 @@ function createItem(name) {
     });
 }
 
+function updateItem(id, updateData) {
+    return fetch(`${baseURL}items/${id}`, {
+    method: 'PATCH', 
+    headers: {
+        'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify(updateData)
+})
+
+}
+
 export default {
     getItems, 
-    createItem
+    createItem, 
+    updateItem
 }
--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ import store from './store'
 import shoppingList from './shopping-list';
 
 api.getItems()
-  .then(res => res.json())
-  .then((items) => {
-    items.forEach((item) => store.addItem(item));
-    shoppingList.render();
-  });
+.then(res => res.json())
+.then((items) => {
+  const item = items[0];
+  return api.updateItem(item.id, { name: 'foobar' });
+})
+.then(res => res.json())
+.then(() => console.log('updated!'));
+
+
 
 const main = function () {
   shoppingList.bindEventListeners();
